71a6151aac72dd06a098db74cce717ca26adaff6
Pankaj Kumar <pnkmrrr@gmail.com>
2018-12-09 01:25:08 +0530

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,11 +7,11 @@
  * @since 1.0.0
  */
 
-if ( ! function_exists( 'twentynineteen_posted_on' ) ) :
+if ( ! function_exists( 'genberg_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
-	function twentynineteen_posted_on() {
+	function genberg_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -27,81 +27,81 @@ if ( ! function_exists( 'twentynineteen_posted_on' ) ) :
 
 		printf(
 			'<span class="posted-on">%1$s<a href="%2$s" rel="bookmark">%3$s</a></span>',
-			twentynineteen_get_icon_svg( 'watch', 16 ),
+			genberg_get_icon_svg( 'watch', 16 ),
 			esc_url( get_permalink() ),
 			$time_string
 		);
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_posted_by' ) ) :
+if ( ! function_exists( 'genberg_posted_by' ) ) :
 	/**
 	 * Prints HTML with meta information about theme author.
 	 */
-	function twentynineteen_posted_by() {
+	function genberg_posted_by() {
 		printf(
 			'<span class="byline">%1$s<span class="screen-reader-text">%2$s</span><span class="author vcard"><a class="url fn n" href="%3$s">%4$s</a></span></span>',
 			/* translators: 1: SVG icon. 2: post author, only visible to screen readers. 3: author link. */
-			twentynineteen_get_icon_svg( 'person', 16 ),
-			__( 'Posted by', 'twentynineteen' ),
+			genberg_get_icon_svg( 'person', 16 ),
+			__( 'Posted by', 'genberg' ),
 			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 			esc_html( get_the_author() )
 		);
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_comment_count' ) ) :
+if ( ! function_exists( 'genberg_comment_count' ) ) :
 	/**
 	 * Prints HTML with the comment count for the current post.
 	 */
-	function twentynineteen_comment_count() {
+	function genberg_comment_count() {
 		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
-			echo twentynineteen_get_icon_svg( 'comment', 16 );
+			echo genberg_get_icon_svg( 'comment', 16 );
 
 			/* translators: %s: Name of current post. Only visible to screen readers. */
-			comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'twentynineteen' ), get_the_title() ) );
+			comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'genberg' ), get_the_title() ) );
 
 			echo '</span>';
 		}
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_entry_footer' ) ) :
+if ( ! function_exists( 'genberg_entry_footer' ) ) :
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
-	function twentynineteen_entry_footer() {
+	function genberg_entry_footer() {
 
 		// Hide author, post date, category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 
 			// Posted by
-			twentynineteen_posted_by();
+			genberg_posted_by();
 
 			// Posted on
-			twentynineteen_posted_on();
+			genberg_posted_on();
 
 			/* translators: used between list items, there is a space after the comma. */
-			$categories_list = get_the_category_list( __( ', ', 'twentynineteen' ) );
+			$categories_list = get_the_category_list( __( ', ', 'genberg' ) );
 			if ( $categories_list ) {
 				/* translators: 1: SVG icon. 2: posted in label, only visible to screen readers. 3: list of categories. */
 				printf(
 					'<span class="cat-links">%1$s<span class="screen-reader-text">%2$s</span>%3$s</span>',
-					twentynineteen_get_icon_svg( 'archive', 16 ),
-					__( 'Posted in', 'twentynineteen' ),
+					genberg_get_icon_svg( 'archive', 16 ),
+					__( 'Posted in', 'genberg' ),
 					$categories_list
 				); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma. */
-			$tags_list = get_the_tag_list( '', __( ', ', 'twentynineteen' ) );
+			$tags_list = get_the_tag_list( '', __( ', ', 'genberg' ) );
 			if ( $tags_list ) {
 				/* translators: 1: SVG icon. 2: posted in label, only visible to screen readers. 3: list of tags. */
 				printf(
 					'<span class="tags-links">%1$s<span class="screen-reader-text">%2$s </span>%3$s</span>',
-					twentynineteen_get_icon_svg( 'tag', 16 ),
-					__( 'Tags:', 'twentynineteen' ),
+					genberg_get_icon_svg( 'tag', 16 ),
+					__( 'Tags:', 'genberg' ),
 					$tags_list
 				); // WPCS: XSS OK.
 			}
@@ -109,7 +109,7 @@ if ( ! function_exists( 'twentynineteen_entry_footer' ) ) :
 
 		// Comment count.
 		if ( ! is_singular() ) {
-			twentynineteen_comment_count();
+			genberg_comment_count();
 		}
 
 		// Edit post link.
@@ -117,7 +117,7 @@ if ( ! function_exists( 'twentynineteen_entry_footer' ) ) :
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'twentynineteen' ),
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'genberg' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -126,21 +126,21 @@ if ( ! function_exists( 'twentynineteen_entry_footer' ) ) :
 				),
 				get_the_title()
 			),
-			'<span class="edit-link">' . twentynineteen_get_icon_svg( 'edit', 16 ),
+			'<span class="edit-link">' . genberg_get_icon_svg( 'edit', 16 ),
 			'</span>'
 		);
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_post_thumbnail' ) ) :
+if ( ! function_exists( 'genberg_post_thumbnail' ) ) :
 	/**
 	 * Displays an optional post thumbnail.
 	 *
 	 * Wraps the post thumbnail in an anchor element on index views, or a div
 	 * element when on single views.
 	 */
-	function twentynineteen_post_thumbnail() {
-		if ( ! twentynineteen_can_show_post_thumbnail() ) {
+	function genberg_post_thumbnail() {
+		if ( ! genberg_can_show_post_thumbnail() ) {
 			return;
 		}
 
@@ -168,25 +168,25 @@ if ( ! function_exists( 'twentynineteen_post_thumbnail' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_comment_avatar' ) ) :
+if ( ! function_exists( 'genberg_comment_avatar' ) ) :
 	/**
 	 * Returns the HTML markup to generate a user avatar.
 	 */
-	function twentynineteen_get_user_avatar_markup( $id_or_email = null ) {
+	function genberg_get_user_avatar_markup( $id_or_email = null ) {
 
 		if ( ! isset( $id_or_email ) ) {
 			$id_or_email = get_current_user_id();
 		}
 
-		return sprintf( '<div class="comment-user-avatar comment-author vcard">%s</div>', get_avatar( $id_or_email, twentynineteen_get_avatar_size() ) );
+		return sprintf( '<div class="comment-user-avatar comment-author vcard">%s</div>', get_avatar( $id_or_email, genberg_get_avatar_size() ) );
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_discussion_avatars_list' ) ) :
+if ( ! function_exists( 'genberg_discussion_avatars_list' ) ) :
 	/**
 	 * Displays a list of avatars involved in a discussion for a given post.
 	 */
-	function twentynineteen_discussion_avatars_list( $comment_authors ) {
+	function genberg_discussion_avatars_list( $comment_authors ) {
 		if ( empty( $comment_authors ) ) {
 			return;
 		}
@@ -194,18 +194,18 @@ if ( ! function_exists( 'twentynineteen_discussion_avatars_list' ) ) :
 		foreach ( $comment_authors as $id_or_email ) {
 			printf(
 				"<li>%s</li>\n",
-				twentynineteen_get_user_avatar_markup( $id_or_email )
+				genberg_get_user_avatar_markup( $id_or_email )
 			);
 		}
 		echo '</ol><!-- .discussion-avatar-list -->', "\n";
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_comment_form' ) ) :
+if ( ! function_exists( 'genberg_comment_form' ) ) :
 	/**
 	 * Documentation for function.
 	 */
-	function twentynineteen_comment_form( $order ) {
+	function genberg_comment_form( $order ) {
 		if ( true === $order || strtolower( $order ) === strtolower( get_option( 'comment_order', 'asc' ) ) ) {
 
 			comment_form(
@@ -218,18 +218,18 @@ if ( ! function_exists( 'twentynineteen_comment_form' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_the_posts_navigation' ) ) :
+if ( ! function_exists( 'genberg_the_posts_navigation' ) ) :
 	/**
 	 * Documentation for function.
 	 */
-	function twentynineteen_the_posts_navigation() {
-		$prev_icon = twentynineteen_get_icon_svg( 'chevron_left', 22 );
-		$next_icon = twentynineteen_get_icon_svg( 'chevron_right', 22 );
+	function genberg_the_posts_navigation() {
+		$prev_icon = genberg_get_icon_svg( 'chevron_left', 22 );
+		$next_icon = genberg_get_icon_svg( 'chevron_right', 22 );
 		the_posts_pagination(
 			array(
 				'mid_size'  => 2,
-				'prev_text' => sprintf( '%s <span class="nav-prev-text">%s</span>', $prev_icon, __( 'Newer posts', 'twentynineteen' ) ),
-				'next_text' => sprintf( '<span class="nav-next-text">%s</span> %s', __( 'Older posts', 'twentynineteen' ), $next_icon ),
+				'prev_text' => sprintf( '%s <span class="nav-prev-text">%s</span>', $prev_icon, __( 'Newer posts', 'genberg' ) ),
+				'next_text' => sprintf( '<span class="nav-next-text">%s</span> %s', __( 'Older posts', 'genberg' ), $next_icon ),
 			)
 		);
 	}
