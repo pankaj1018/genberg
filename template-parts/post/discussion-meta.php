1bdd56e324cdfe81170f091b71415e007424d203
Pankaj Kumar <pnkmrrr@gmail.com>
2018-12-09 01:25:46 +0530

    Update discussion-meta.php

--- template-parts/post/discussion-meta.php
+++ template-parts/post/discussion-meta.php
@@ -8,14 +8,14 @@
  */
 
 /* Get data from current discussion on post. */
-$discussion    = twentynineteen_get_discussion_data();
+$discussion    = genberg_get_discussion_data();
 $has_responses = $discussion->responses > 0;
 
 if ( $has_responses ) {
 	/* translators: %1(X comments)$s */
-	$meta_label = sprintf( _n( '%d Comment', '%d Comments', $discussion->responses, 'twentynineteen' ), $discussion->responses );
+	$meta_label = sprintf( _n( '%d Comment', '%d Comments', $discussion->responses, 'genberg' ), $discussion->responses );
 } else {
-	$meta_label = __( 'No comments', 'twentynineteen' );
+	$meta_label = __( 'No comments', 'genberg' );
 }
 
 ?>
@@ -23,11 +23,11 @@ if ( $has_responses ) {
 <div class="discussion-meta">
 	<?php
 	if ( $has_responses ) {
-		twentynineteen_discussion_avatars_list( $discussion->authors );
+		genberg_discussion_avatars_list( $discussion->authors );
 	}
 	?>
 	<p class="discussion-meta-info">
-		<?php echo twentynineteen_get_icon_svg( 'comment', 24 ); ?>
+		<?php echo genberg_get_icon_svg( 'comment', 24 ); ?>
 		<span><?php echo esc_html( $meta_label ); ?></span>
 	</p>
 </div><!-- .discussion-meta -->
