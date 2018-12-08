da35f09eacae15b2f85cbf110bfbd0eff1996957
Pankaj Kumar <pnkmrrr@gmail.com>
2018-12-09 01:24:56 +0530

    Update back-compat.php

--- inc/back-compat.php
+++ inc/back-compat.php
@@ -18,12 +18,12 @@
  *
  * @since Twenty Nineteen 1.0.0
  */
-function twentynineteen_switch_theme() {
+function genberg_switch_theme() {
 	switch_theme( WP_DEFAULT_THEME );
 	unset( $_GET['activated'] );
-	add_action( 'admin_notices', 'twentynineteen_upgrade_notice' );
+	add_action( 'admin_notices', 'genberg_upgrade_notice' );
 }
-add_action( 'after_switch_theme', 'twentynineteen_switch_theme' );
+add_action( 'after_switch_theme', 'genberg_switch_theme' );
 
 /**
  * Adds a message for unsuccessful theme switch.
@@ -35,8 +35,8 @@ add_action( 'after_switch_theme', 'twentynineteen_switch_theme' );
  *
  * @global string $wp_version WordPress version.
  */
-function twentynineteen_upgrade_notice() {
-	$message = sprintf( __( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'twentynineteen' ), $GLOBALS['wp_version'] );
+function genberg_upgrade_notice() {
+	$message = sprintf( __( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'genberg' ), $GLOBALS['wp_version'] );
 	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
@@ -47,10 +47,10 @@ function twentynineteen_upgrade_notice() {
  *
  * @global string $wp_version WordPress version.
  */
-function twentynineteen_customize() {
+function genberg_customize() {
 	wp_die(
 		sprintf(
-			__( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'twentynineteen' ),
+			__( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'genberg' ),
 			$GLOBALS['wp_version']
 		),
 		'',
@@ -59,7 +59,7 @@ function twentynineteen_customize() {
 		)
 	);
 }
-add_action( 'load-customize.php', 'twentynineteen_customize' );
+add_action( 'load-customize.php', 'genberg_customize' );
 
 /**
  * Prevents the Theme Preview from being loaded on WordPress versions prior to 4.7.
@@ -68,9 +68,9 @@ add_action( 'load-customize.php', 'twentynineteen_customize' );
  *
  * @global string $wp_version WordPress version.
  */
-function twentynineteen_preview() {
+function genberg_preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		wp_die( sprintf( __( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'twentynineteen' ), $GLOBALS['wp_version'] ) );
+		wp_die( sprintf( __( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'genberg' ), $GLOBALS['wp_version'] ) );
 	}
 }
-add_action( 'template_redirect', 'twentynineteen_preview' );
+add_action( 'template_redirect', 'genberg_preview' );
