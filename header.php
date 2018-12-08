96b038af37b7960634de117e8fa94680b9e62870
Pankaj Kumar <pnkmrrr@gmail.com>
2018-12-09 01:24:49 +0530

    Update header.php

--- header.php
+++ header.php
@@ -21,19 +21,19 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'genberg' ); ?></a>
 
-		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+		<header id="masthead" class="<?php echo is_singular() && genberg_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
 
 			<div class="site-branding-container">
 				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 			</div><!-- .layout-wrap -->
 
-			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
+			<?php if ( is_singular() && genberg_can_show_post_thumbnail() ) : ?>
 				<div class="site-featured-image">
-					<?php twentynineteen_post_thumbnail(); ?>
+					<?php genberg_post_thumbnail(); ?>
 					<?php the_post(); ?>
-					<?php $discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; ?>
+					<?php $discussion = ! is_page() && genberg_can_show_post_thumbnail() ? genberg_get_discussion_data() : null; ?>
 					<div class="<?php echo ( ! empty( $discussion ) && count( $discussion->responses ) > 0 ) ? 'entry-header has-discussion' : 'entry-header'; ?>">
 						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
 					</div><!-- .entry-header -->
