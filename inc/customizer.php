d46f1515ece669c471e85cc9c523d5abaa567c1c
Pankaj Kumar <pnkmrrr@gmail.com>
2018-12-09 01:25:01 +0530

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -12,7 +12,7 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function twentynineteen_customize_register( $wp_customize ) {
+function genberg_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
@@ -22,14 +22,14 @@ function twentynineteen_customize_register( $wp_customize ) {
 			'blogname',
 			array(
 				'selector'        => '.site-title a',
-				'render_callback' => 'twentynineteen_customize_partial_blogname',
+				'render_callback' => 'genberg_customize_partial_blogname',
 			)
 		);
 		$wp_customize->selective_refresh->add_partial(
 			'blogdescription',
 			array(
 				'selector'        => '.site-description',
-				'render_callback' => 'twentynineteen_customize_partial_blogdescription',
+				'render_callback' => 'genberg_customize_partial_blogdescription',
 			)
 		);
 	}
@@ -42,7 +42,7 @@ function twentynineteen_customize_register( $wp_customize ) {
 		array(
 			'default'           => 'default',
 			'transport'         => 'postMessage',
-			'sanitize_callback' => 'twentynineteen_sanitize_color_option',
+			'sanitize_callback' => 'genberg_sanitize_color_option',
 		)
 	);
 
@@ -50,10 +50,10 @@ function twentynineteen_customize_register( $wp_customize ) {
 		'primary_color',
 		array(
 			'type'     => 'radio',
-			'label'    => __( 'Primary Color', 'twentynineteen' ),
+			'label'    => __( 'Primary Color', 'genberg' ),
 			'choices'  => array(
-				'default'  => _x( 'Default', 'primary color', 'twentynineteen' ),
-				'custom' => _x( 'Custom', 'primary color', 'twentynineteen' ),
+				'default'  => _x( 'Default', 'primary color', 'genberg' ),
+				'custom' => _x( 'Custom', 'primary color', 'genberg' ),
 			),
 			'section'  => 'colors',
 			'priority' => 5,
@@ -75,7 +75,7 @@ function twentynineteen_customize_register( $wp_customize ) {
 			$wp_customize,
 			'primary_color_hue',
 			array(
-				'description' => __( 'Apply a custom color for buttons, links, featured images, etc.', 'twentynineteen' ),
+				'description' => __( 'Apply a custom color for buttons, links, featured images, etc.', 'genberg' ),
 				'section'     => 'colors',
 				'mode'        => 'hue',
 			)
@@ -95,20 +95,20 @@ function twentynineteen_customize_register( $wp_customize ) {
 	$wp_customize->add_control(
 		'image_filter',
 		array(
-			'label'   => __( 'Apply a filter to featured images using the primary color', 'twentynineteen' ),
+			'label'   => __( 'Apply a filter to featured images using the primary color', 'genberg' ),
 			'section' => 'colors',
 			'type'    => 'checkbox',
 		)
 	);
 }
-add_action( 'customize_register', 'twentynineteen_customize_register' );
+add_action( 'customize_register', 'genberg_customize_register' );
 
 /**
  * Render the site title for the selective refresh partial.
  *
  * @return void
  */
-function twentynineteen_customize_partial_blogname() {
+function genberg_customize_partial_blogname() {
 	bloginfo( 'name' );
 }
 
@@ -117,25 +117,25 @@ function twentynineteen_customize_partial_blogname() {
  *
  * @return void
  */
-function twentynineteen_customize_partial_blogdescription() {
+function genberg_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
 /**
  * Bind JS handlers to instantly live-preview changes.
  */
-function twentynineteen_customize_preview_js() {
-	wp_enqueue_script( 'twentynineteen-customize-preview', get_theme_file_uri( '/js/customize-preview.js' ), array( 'customize-preview' ), '20151215', true );
+function genberg_customize_preview_js() {
+	wp_enqueue_script( 'genberg-customize-preview', get_theme_file_uri( '/js/customize-preview.js' ), array( 'customize-preview' ), '20151215', true );
 }
-add_action( 'customize_preview_init', 'twentynineteen_customize_preview_js' );
+add_action( 'customize_preview_init', 'genberg_customize_preview_js' );
 
 /**
  * Load dynamic logic for the customizer controls area.
  */
-function twentynineteen_panels_js() {
-	wp_enqueue_script( 'twentynineteen-customize-controls', get_theme_file_uri( '/js/customize-controls.js' ), array(), '1.0', true );
+function genberg_panels_js() {
+	wp_enqueue_script( 'genberg-customize-controls', get_theme_file_uri( '/js/customize-controls.js' ), array(), '1.0', true );
 }
-add_action( 'customize_controls_enqueue_scripts', 'twentynineteen_panels_js' );
+add_action( 'customize_controls_enqueue_scripts', 'genberg_panels_js' );
 
 /**
  * Sanitize custom color choice.
@@ -144,7 +144,7 @@ add_action( 'customize_controls_enqueue_scripts', 'twentynineteen_panels_js' );
  *
  * @return string
  */
-function twentynineteen_sanitize_color_option( $choice ) {
+function genberg_sanitize_color_option( $choice ) {
 	$valid = array(
 		'default',
 		'custom',
