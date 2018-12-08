cc46278697f68589efd82630b675843821b71a01
Pankaj Kumar <pnkmrrr@gmail.com>
2018-12-09 01:25:25 +0530

    Update content-excerpt.php

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -15,19 +15,19 @@
 	<header class="entry-header">
 		<?php
 		if ( is_sticky() && is_home() && ! is_paged() ) {
-			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
+			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'genberg' ) );
 		}
 		the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 		?>
 	</header><!-- .entry-header -->
 
-	<?php twentynineteen_post_thumbnail(); ?>
+	<?php genberg_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php twentynineteen_entry_footer(); ?>
+		<?php genberg_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-${ID} -->
